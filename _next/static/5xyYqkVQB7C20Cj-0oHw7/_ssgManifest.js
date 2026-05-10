62c0a65854f7cc8ebbdcdf1cdc4a55538c666f57
Surgbc <Surgbc@users.noreply.github.com>
2026-05-10 10:53:49 +0000

    deploy: brianmechanisms/brianmechanisms-designer@57fbf121b125ff568486c5519fbf76ff272618c9

--- _next/static/5xyYqkVQB7C20Cj-0oHw7/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002Fcategories\u002F[id]","\u002Fmechanisms\u002F[category]\u002F[id]","\u002Fpractical\u002F[id]","\u002Fresearch\u002F[slug]","\u002Fsynthesized\u002Fapp\u002F[id]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
